89b1e5bb44d06f259aa5fc200b9f1c8db9d048bd
GABRIEL KREMER DE SOUSA <gabriel.sousa2001@cs.cruzeirodosul.edu.br>
2025-07-03 09:40:54 -0300

    Criação do POST

--- BIBLIOTECA/API/Program.cs
+++ BIBLIOTECA/API/Program.cs
@@ -10,6 +10,19 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
+app.MapPost("/api/livros", async ([FromServices] BibliotecaDbContext db, [FromBody] Livro livro) =>
+{
+    if(livro.Titulo is null || livro.Titulo.Length < 3) {
+        return Results.BadRequest("Erro, Titulo tem menos de 3 Caracteres");
+    }
+    if(livro.Autor is null || livro.Autor.Length < 3) {
+        return Results.BadRequest("Erro, Autor tem menos de 3 Caracteres");
+    }
 
+    db.Livros.Add(livro);
+    await db.SaveChangesAsync();
+
+    return Results.Created($"/api/livros/{livro.Id}", livro);
+});
 
 app.Run();
